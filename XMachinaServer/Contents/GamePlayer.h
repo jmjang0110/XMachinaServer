10ba2f987501b526abf23924353a2cda038616a8
SaintPingu <45813433+SaintPingu@users.noreply.github.com>
2024-08-29 11:18:08 +0900

    server for cinema

--- XMachinaServer/Contents/GamePlayer.h
+++ XMachinaServer/Contents/GamePlayer.h
@@ -71,7 +71,7 @@ private:
 	Concurrency::concurrent_queue<int>									mPossibleBulletIndex;		// Possible To Shoot Bullets 
 	
 	// View List
-	float					mViewRangeRadius = 45.f;										// View Range 
+	float					mViewRangeRadius = 190.f;										// View Range 
 	ViewList				mVlist           = {};										// View List ( Current ) 
 public:
 	GamePlayer();
